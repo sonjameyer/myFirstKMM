e1072963c3feeb54f175e797351da1c9c358a0a7
Sonja <sonja99meyer@gmail.com>
2021-05-17 15:05:58 +0200

    Latest

--- androidKMM/src/main/java/com/skynamo/myfirstkmm/androidKMM/MainActivity.kt
+++ androidKMM/src/main/java/com/skynamo/myfirstkmm/androidKMM/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.skynamo.myfirstkmm.androidKMM
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.skynamo.myfirstkmm.sharedKMM.Greeting
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.skynamo.myfirstkmm.sharedKMM.Convert
+import com.skynamo.myfirstkmm.sharedKMM.Greeting
 
 fun greet(): String {
     return Greeting().greeting()
@@ -13,9 +17,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setTitle("Cooking Converter")
+        setTitle("Convert the British")
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        val ounceForGramsAnswer: TextView = findViewById(R.id.tvOunceToGramsAnswer)
+        val ounceForGramsEditText: EditText = findViewById(R.id.etOunceForGrams)
+        ounceForGramsEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val number: Double = s.toString().toDouble()
+                ounceForGramsAnswer.text = Convert().ounceToGram(number).toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
     }
 }
